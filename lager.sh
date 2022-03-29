27b8710a9983fc99cffeefcee5dffaad8ba313a5
John Davis <johnmdavis125@gmail.com>
2022-03-29 03:29:15 -0400

    test multi-line eval script on newly instantiated lagerDB

--- lager.sh
+++ lager.sh
@@ -1,2 +1,10 @@
 #!/bin/zsh
-echo "hello world!"
+echo "do some stuff"
+mongo --eval "
+    db = db.getSiblingDB('lagerDB'); 
+    db.records.insertOne({'userName': 'johndavis', 'date': '29 Mar 2022', 'category': 'Coding Projects', 'Duration': '60', 'timeOfEntry': '0000' }); 
+    db.records.insertOne({'userName': 'johndavis', 'date': '29 Mar 2022', 'category': 'Coding Projects', 'Duration': '60', 'timeOfEntry': '0000' }); 
+    db.records.find({}).forEach(printjson); 
+"
+
+echo "do some more stuff"
