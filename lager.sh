3d843e801ecd9dd27327ae51003585e401aa4a97
John Davis <johnmdavis125@gmail.com>
2022-03-29 08:59:29 -0400

    create user prompts, format response into db insertion, test positive create action

--- lager.sh
+++ lager.sh
@@ -1,10 +1,43 @@
 #!/bin/zsh
-echo "do some stuff"
+echo ''
+echo ''
+echo ''
+genTimestamp() {
+        TIMESTAMP=$(date)
+    }
+
+echo "Welcome to Lager! What'll it be?" 
+read "?'c' to create a new record, 's' to search existing records: " CHOSEN_ACTION
+
+if [ '$CHOSEN_ACTION'='c' ]
+then
+    typeset -A CATEGORIES
+    CATEGORIES=([cp]=code_projects [cc]=coding_course [hh]=holy_hour [wo]=work_out [kp]=KDP [gp]=guitar_practice [ph]=photography_videography [ps]=photoshop [dn]=design [ts]=tee_shirt_business [ct]=compTia [re]=real_estate [dl]=Dev_Log [el]=electronics_PCB [rd]=reading_books) 
+
+    echo "CREATE NEW RECORD"
+    echo "Category Options:"
+    for KEY VALUE in ${(kv)CATEGORIES}
+    do
+        echo "$KEY: $VALUE"
+    done
+    
+    read "?Enter the 2-letter appropriate 2-letter key code: " KEY_CODE
+
+    ENTRY_CATEGORY=${CATEGORIES[$KEY_CODE]}
+    echo "Selected Category: $ENTRY_CATEGORY"
+
+    read "?Duration? (min): " DURATION
+
+    USER_NAME=$(whoami)
+    echo Username: $USER_NAME
+
+    genTimestamp
+fi
+
 mongo --eval "
     db = db.getSiblingDB('lagerDB'); 
-    db.records.insertOne({'userName': 'johndavis', 'date': '29 Mar 2022', 'category': 'Coding Projects', 'Duration': '60', 'timeOfEntry': '0000' }); 
-    db.records.insertOne({'userName': 'johndavis', 'date': '29 Mar 2022', 'category': 'Coding Projects', 'Duration': '60', 'timeOfEntry': '0000' }); 
+    db.records.insertOne({'userName': '$USER_NAME', 'category': '$ENTRY_CATEGORY', 'Duration': '$DURATION', 'Timestamp': '$TIMESTAMP' }); 
     db.records.find({}).forEach(printjson); 
 "
 
-echo "do some more stuff"
+
